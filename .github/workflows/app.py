dfd6ff4f27993f74392177d4ebe896147c123e3f
Jsyusco <jason.salinas@yusco.io>
2025-11-28 16:36:02 +0100

    Update app.py

--- .github/workflows/app.py
+++ .github/workflows/app.py
@@ -370,21 +370,11 @@ if uploaded_file is not None:
                                 is_valid_phase = False
                                 missing_phase.extend(missing_sec)
 
-                       if st.button("✅ Démarrer une nouvelle phase"):
-                            # Incrémente la phase
+                  if st.button("✅ Démarrer une nouvelle phase"):
                             st.session_state['current_phase'] += 1
                             new_phase_key = get_current_phase_key()
                             st.session_state['form_answers'][new_phase_key] = {}
-                        
-                            # Redirige vers la section de sélection de phase
-                            target_section = find_phase_selection_section(df)
-                            visible_sections = [sec for sec in df['section'].unique() if check_section_condition(df[df['section']==sec], {})]
-                        
-                            if target_section and target_section in visible_sections:
-                                st.session_state['current_section_index'] = visible_sections.index(target_section)
-                            else:
-                                st.session_state['current_section_index'] = 0
-                        
+                            st.session_state['current_section_index'] = 0
                             st.rerun()
 
                         else:
