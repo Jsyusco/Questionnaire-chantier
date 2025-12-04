24a2b05c29dad912c53fb4214b5db62a5dcdc5fb
Jsyusco <jason.salinas@yusco.io>
2025-12-04 14:54:31 +0100

    Update app.py

--- .github/workflows/app.py
+++ .github/workflows/app.py
@@ -436,7 +436,7 @@ elif st.session_state['step'] == 'PROJECT':
         st.error("Colonne 'Intitulé' manquante.")
     else:
         
-        search_term = st.text_input("Rechercher un projet (3 caractères minimum)", key="project_search_input").strip()
+        search_term = st.text_input("Rechercher un projet (Compléter 3 caractères du nom de la ville minimum)", key="project_search_input").strip()
 
         filtered_projects = []
         selected_proj = None
