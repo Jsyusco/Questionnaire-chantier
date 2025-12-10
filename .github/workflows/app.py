1b5a30d66e767be095c3630cbcfdb3aab64b0296
Jsyusco <jason.salinas@yusco.io>
2025-12-10 15:24:51 +0100

    Update app.py

--- .github/workflows/app.py
+++ .github/workflows/app.py
@@ -711,8 +711,7 @@ elif st.session_state['step'] in ['LOOP_DECISION', 'FILL_PHASE']:
                 if st.button("🏁 Terminer l'audit"):
                     st.session_state['step'] = 'FINISHED'
                     st.rerun()
-                      
-            st.markdown('</div>', unsafe_allow_html=True)
+           st.markdown('</div>', unsafe_allow_html=True)
 
 
     elif st.session_state['step'] == 'FILL_PHASE':
